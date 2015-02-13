7b67a9929549233e6d07a139f8b35982b6121e0e
jtrecenti <julio.trecenti@gmail.com>
2015-02-13 11:29:03 -0200

    simplificando tudo

--- server.R
+++ server.R
@@ -1,39 +1,23 @@
-
-# This is the server logic for a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 library(ggplot2)
 
-# a função abaixo tem dois parâmetros:
-# y chr nome da variável resposta
-# x chr vetor com nomes das variáveis explicativas
-# a função retorna um modelo de regressão linear ajustado
+# Ajusta modelo para as variaveis y e x no banco de dados diamonds
 ajustar_modelo <- function(y, x){
-  form <- as.formula(paste(y, "~", paste(x, collapse = "+")))
+  form <- as.formula(paste(y, x, sep='~'))
   modelo <- lm(form, data = diamonds)
   return(modelo)
 }
 
-
 shinyServer(function(input, output) {
-  
+      
   output$reta_ajustada <- renderPlot({
     
     # definição das variáveis 
     y <- input$resposta
-    x <- input$covariaveis
-    
-    eixo_x <- input$eixo_x
+    x <- input$x
     
     # faça um gráfico da variável resposta pela variável selecionada para o eixo X
     # inclua a reta de regressão ajustada.
-    # se a variável selecionada for categorica faça um boxplot da resposta para cada nível.
-    
-    
     
   })
   
--- ui.R
+++ ui.R
@@ -1,40 +1,23 @@
-
-# This is the user-interface definition of a Shiny web application.
-# You can find out more about building applications with Shiny here:
-#
-# http://shiny.rstudio.com
-#
-
 library(shiny)
 library(ggplot2)
 
 
-shinyUI(fluidPage(
+fluidPage(  
   
-  # Application title
   titlePanel("Laboratório IV"),
   
-  # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      # coloque aqui um seletor de qual variável do banco de dados
-      # diamonds será a resposta
-      selectInput(inputId = "resposta", #.... termine de preencher)
-      
-      # coloque aqui um seletor múltiplo de todas as variáveis explicativas
-      # do modelo
-      
-      
-      
-      # coloque aqui um seletor de qual variável corresponderá ao eixo X do gráfico
-      
-      
-      
+      # coloque aqui um seletor de qual variável do banco de dados será a resposta
+      # (somente 'price', 'carat', 'x', 'y', 'z')
+            
+      # coloque aqui um seletor de qual variável será a explicativa
+      # (somente 'price', 'carat', 'x', 'y', 'z')
     ),
     
-    # Show a plot of the generated distribution
     mainPanel(
+      tags$h2('Gráfico!'),
       plotOutput("reta_ajustada")
     )
   )
-))
+)
